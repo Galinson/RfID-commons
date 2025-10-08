0e618fe912deb874402ba2326e1cc8417f84cc17
Galinson <giannispetropoulos318@gmail.com>
2025-10-08 18:35:25 +0300

    Update RFIDReader.h

--- RFIDReader.h
+++ RFIDReader.h
@@ -1,1 +1,22 @@
+#ifndef RFIDREADER_H
+#define RFIDREADER_H
+
+#include <Arduino.h>
+#include <SPI.h>
+#include <MFRC522.h>  // The standard RFID driver library
+
+class RFIDReader {
+  private:
+    MFRC522 rfid;     // Instance of the RFID driver
+    byte ssPin;       // Slave Select pin
+    byte rstPin;      // Reset pin
+
+  public:
+    RFIDReader(byte ssPin, byte rstPin);  // Constructor
+    void begin();                         // Initialize the reader
+    bool isCardPresent();                 // Check if a card is detected
+    String readUID();                     // Read card UID as string
+};
+
+#endif
 
